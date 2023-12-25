231326bf7e6f07e29833babd1486d60f5a1228d4
Zhang, Zehong <zhangbene2019@gmail>
2023-12-25 20:28:42 +0100

    v1.1

--- subdatabase_extract.py
+++ subdatabase_extract.py
@@ -3,7 +3,61 @@ from tkinter import filedialog
 import pandas as pd
 from Bio import SeqIO
 
-def process_files():
+def error_back():
+    error_back_window = tk.Toplevel(root)
+    error_back_window.title("Error")
+
+    def restart_and_close():
+        restart()
+        error_back_window.destroy()
+
+    error_label = tk.Label(error_back_window, text="Error in data identification, \nplease restart with other data \nor contact the author.")
+    error_label.pack()
+
+    button_restart = tk.Button(error_back_window, text="Restart", command=restart_and_close, height=1, width=20)
+    button_restart.pack(pady=(5, 15), padx=(15, 20))
+
+
+def show_asking_window():
+    protein_groups_path = entry_protein_groups.get()
+    df = pd.read_table(protein_groups_path, sep="\t")
+    if "Majority protein IDs" in df.columns and "Accession" not in df.columns:
+        analyzer_software = "MaxQuant"
+    elif "Majority protein IDs" not in df.columns and "Accession" in df.columns:
+        analyzer_software = "Thermo Proteome Discoverer"
+
+    asking_window = tk.Toplevel(root)
+    asking_window.title("Confirm")
+
+    def yes_action():
+        process_files("Majority protein IDs" if analyzer_software == "MaxQuant" else "Accession")
+        asking_window.destroy()
+
+    def no_action():
+        error_back()
+        asking_window.destroy()
+
+    def not_sure_action():
+        process_files("Majority protein IDs" if analyzer_software == "MaxQuant" else "Accession")
+        asking_window.destroy()
+
+    asking_label = tk.Label(asking_window, text=f"Do this result file from {analyzer_software}")
+    asking_label.grid(row=0,column=2)
+
+    button1 = tk.Button(asking_window, text="Yes", command=yes_action)
+    button1.grid(row=1, column=1, pady=(5, 15), padx=(15, 20))
+
+    button2 = tk.Button(asking_window, text="No", command=no_action)
+    button2.grid(row=1, column=2, pady=(5, 15), padx=(15, 20))
+
+    button3 = tk.Button(asking_window, text="Not Sure", command=not_sure_action)
+    button3.grid(row=1, column=3, pady=(5, 15), padx=(15, 20))
+
+    
+
+def process_files(column_name: str):
+    """Input is the protein id's column name"""
+    # need to be modified, add PD format
     protein_groups_path = entry_protein_groups.get()
     fasta_file_path = entry_fasta_file.get()
 
@@ -13,7 +67,7 @@ def process_files():
 
     df = pd.read_table(protein_groups_path, sep="\t")
 
-    uniprot_id_list = [prot.strip() for proteins in df['Majority protein IDs'].to_list() for prot in proteins.split(";")]
+    uniprot_id_list = [prot.strip() for proteins in df[column_name].to_list() for prot in proteins.split(";")]
 
     fasta_file = SeqIO.parse(fasta_file_path, "fasta")
     my_records = []
@@ -57,7 +111,7 @@ entry_fasta_file.grid(row=1, column=1, pady=(10, 0))
 button_browse_fasta_file = tk.Button(root, text="Browse", command=lambda: entry_fasta_file.insert(0, filedialog.askopenfilename()),width=10)
 button_browse_fasta_file.grid(row=1, column=2, pady=(10, 0), padx=(15, 20))
 
-button_run = tk.Button(root, text="Run", command=process_files, height=1, width=10)  # Adjust height and width
+button_run = tk.Button(root, text="Run", command=show_asking_window, height=1, width=10)  # Adjust height and width
 button_run.grid(row=2, column=2, pady=(20, 20), padx=(15, 20))
 
 label_status_file = tk.Label(root, text="Running result:",width=20)
--- test_file.py
+++ test_file.py
@@ -1,17 +1,27 @@
 import tkinter as tk
 
+def show_tip(text_show):
+    wd_tip = tk.Toplevel(root)
+    wd_tip.title("tip")
+
+    tip_label = tk.Label(wd_tip, text=text_show)
+    tip_label.pack()
+
+    button_back = tk.Button(wd_tip, text = "Back")
+    button_back.pack()
+
 def show_window1():
     window1 = tk.Toplevel(root)
     window1.title("Window 1")
 
     def on_choice1():
-        result_label.config(text="You chose 1")
+        show_tip("You chose 1")
 
     def on_choice2():
-        result_label.config(text="2")
+        show_tip("2")
 
     def on_choice3():
-        result_label.config(text="Please do not choose it")
+        show_tip("Please do not choose it")
 
     button1 = tk.Button(window1, text="Choose 1", command=on_choice1)
     button1.pack()
@@ -22,8 +32,7 @@ def show_window1():
     button3 = tk.Button(window1, text="Choose 3", command=on_choice3)
     button3.pack()
 
-    result_label = tk.Label(window1, text="")
-    result_label.pack()
+    
 
 def show_window2():
     window2 = tk.Toplevel(root)
